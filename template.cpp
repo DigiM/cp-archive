d7b0d2b7b8306be72c45ebbb53be90b283ff9ce2
Degoldie Sonny <degoldie_then@yahoo.com>
2022-09-11 14:25:44 +0800

    add template file

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+#define fi first
+#define se second
+#define mp make_pair
+
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+
+void solve() {
+    
+}
+
+int main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+
+    int qt = 1;
+    // scanf("%d", &qt);
+    // cin >> qt;
+    
+    for (int q = 0; q < qt; q++) {
+        // printf("Case #%d: ", q+1);
+        // cout << "Case #" << q+1 << ": ";
+        solve();
+    }
+}
